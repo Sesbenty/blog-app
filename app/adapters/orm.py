b5fc6c0afa2beccfa25ee0c596c72bb1f7ae74c2
Александр Шанин <a.shanin@int-sys.ru>
2025-02-18 18:52:11 +0300

    Add orm for user model

--- app/adapters/orm.py
+++ app/adapters/orm.py
@@ -1,5 +1,22 @@
+from sqlalchemy import Column, Integer, MetaData, String, Table
+from sqlalchemy.orm import registry
 
-from sqlalchemy import MetaData
+from app.domain.model import User
 
 metadata = MetaData()
 
+users_table = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("email", String(50), nullable=False),
+    Column("first_name", String(50)),
+    Column("last_name", String(50)),
+    Column("password", String(255), nullable=False),
+)
+
+
+def start_mappers():
+    mapper_registry = registry()
+
+    mapper_registry.map_imperatively(User, users_table)
