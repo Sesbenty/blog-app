8d20d9031f4501c87c3af2123626fb0fa541dfe5
Александр Шанин <a.shanin@int-sys.ru>
2025-02-18 17:50:14 +0300

    API for auth router

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+
+from app.domain.schemas import UserAuth, UserRegister
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
+
+@auth_router.post("/register/")
+async def register_user(user_data: UserRegister):
+    pass
+
+
+@auth_router.post("/login/")
+async def auth_user(response: Response, user_data: UserAuth):
+    pass
+
+
+@auth_router.post("/logout/")
+async def logout(response: Response):
+    pass
+
+
+@auth_router.post("/me/")
+async def get_me():
+    pass
