182906f77fa1a87ba8398ae0c9dfcc82114bb49c
Александр Шанин <a.shanin@int-sys.ru>
2025-02-19 13:49:41 +0300

    API for blog router

--- app/routes/blog.py
+++ app/routes/blog.py
@@ -1,4 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session 
+
+from app.domain.models.auth import User
+from app.domain.schemas.blog import BlogBase
+from app.routes.dependecies import get_current_user, session_factory
 
 blog_router = APIRouter(prefix="/blog", tags=["Blog"])
 
+
+@blog_router.delete("/")
+async def delete_blog():
+    pass
+
+
+@blog_router.get("/")
+async def get_all_blogs():
+    pass
+
+
+@blog_router.get("/{blog_id}")
+async def get_blog(blog_id: int):
+    pass
+
+
+@blog_router.post("/{blog_id}")
+async def update_blog(blog_id: int):
+    pass
+
+async def create_blog(blod_data: BlogBase, user_data: User = Depends(get_current_user), session: Session = Depends(session_factory)):
+    pass
