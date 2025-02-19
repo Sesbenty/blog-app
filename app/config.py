29820db6682c9cfcc13650bae86f2a8ccfbc349b
Александр Шанин <a.shanin@int-sys.ru>
2025-02-19 14:10:50 +0300

    Added path to database in alembic env

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ import os
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 database_url = f"sqlite:///{base_dir}/data/db.sqlite3"
 
-SECRET_KEY=os.environ.get("SECRET_KEY", "key")
-ALGORITHM=os.environ.get("ALGORITHM", "HS256")
+SECRET_KEY = os.environ.get("SECRET_KEY", "key")
+ALGORITHM = os.environ.get("ALGORITHM", "HS256")
--- migration/env.py
+++ migration/env.py
@@ -7,10 +7,16 @@ from alembic import context
 
 # Target metadata
 from app.adapters.orm import metadata
+# URL database
+from app.config import database_url
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Modyfy config
+# Set url from app path
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
