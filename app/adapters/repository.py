602af422ed5414cfd4f553c0cf503e47669ea306
Sesbenty <sasha.shanin.01@mail.ru>
2025-02-19 18:23:48 +0300

    Added user repository

---
+++ app/adapters/repository.py
@@ -0,0 +1,32 @@
+import abc
+
+from app.domain.models.auth import User
+
+
+
+class AbstractUserRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, user: User):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, id: int) -> User:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_by_email(self, email: str):
+        raise NotImplementedError
+
+
+class SqlAlchemyUserRepository(AbstractUserRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, user: User):
+        self.session.add(user)
+
+    def get(self, id):
+        return self.session.query(User).filter_by(id=id).first()
+
+    def get_by_email(self, email: str):
+        return self.session.query(User).filter_by(email=email).first()
